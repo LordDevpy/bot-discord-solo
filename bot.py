64132891ec4926dfc2e88c5e9e3d8b5fee2fb0e3
LordDevpy <66754197+LordDevpy@users.noreply.github.com>
2020-08-22 12:09:55 -0300

    more optimized and more commands

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import discord
 import dotenv
-from codecs import encode
+from random import choice
 from picselector import menu
 
 dotenv.load_dotenv()
 
 client = discord.Client()
-msg_user = msg = bmsg = pedidouser = respostaid = pedidoid = None
+simounao = choice(['Sim, eu acho!', 'Não, eu não acho.', 'Talvez...'])
+msg_user = msg = bmsg = pedidouser = pedidoid = None
 start = discord.Embed(title='Escolha seu nsfw da zero two! <:zerogun:738732464117121045>: ',
                       description='**1- Imagens\n 2- Gifs\n 3- Imagens realistas\n 4- Ahegao**\n\n AVISO!!! Não nos responsabilizamos pelo seu pedido<:zerotwocrazy:738974003280216194>',
                       color=1752220)
@@ -18,45 +19,51 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-                                      # argumentos:
-    if message.author == client.user: # para não visualizar a msg dos bots
-        return                        #
-    if message.channel.name == 'dm':  # para não responder dms
-        return                        #
+    args = message.content.lower()
+
+    # argumentos para não: responder outro bot ou si mesmo/ não responder dms
+    if message.author == client.user:
+        return
+    if message.channel.name == 'dm':
+        return
 
 
-    if message.content.lower() == 'bom dia':  # comando bom dia
+    if args == 'bom dia':  # comando bom dia
         await message.channel.send('bom dia delícia! <:zerotwolove:738974003443662918>')
 
-
-    if message.content.lower() == '!safadensa':
+    if args == '!safadensa':
         if message.channel.is_nsfw():
             global bmsg, pedidouser, pedidoid, respostaid, msg
             bmsg = await message.channel.send(embed=start)
-            await bmsg.add_reaction('1️⃣') # tagged
-            await bmsg.add_reaction('2️⃣') # gif
-            await bmsg.add_reaction('3️⃣') # realista
-            await bmsg.add_reaction('4️⃣') # ahegao
+            radd = bmsg.add_reaction
+            await radd('1️⃣') # tagged
+            await radd('2️⃣') # gif
+            await radd('3️⃣') # realista
+            await radd('4️⃣') # ahegao
 
             pedidouser = message.author
             pedidoid = message.id
-            respostaid = bmsg.id
             msg = message
         else:
             await message.channel.send(content='Esse chat NÃO é nsfw! <:dissapointment:738974002944409642>')
     
-    if message.content.lower() == 'eduardo':
-        await message.channel.send(content='**E D U A R D O**\n https://cdn.discordapp.com/attachments/497062752473317397/736651644468723842/eduardo_1.mp4', )
+    if args == 'eduardo':
+        await message.channel.send(content="""**E D U A R D O**
+https://cdn.discordapp.com/attachments/497062752473317397/736651644468723842/eduardo_1.mp4""")
 
+    if args.startswith('!pergunta ') and len(args) > 11:
+        global simounao
+        await message.channel.send(simounao)
+        simounao = choice(['Sim, eu acho!', 'Não, eu não acho.', 'Talvez...'])
 
 @client.event
 async def on_reaction_add(reaction, user):
 
     if user != client.user: # se não for o bot
         if user == pedidouser: # se for o dono do request da msg
-            if respostaid == reaction.message.id: # se ele reagiu a msg certa
+            if bmsg.id == reaction.message.id: # se ele reagiu a msg certa
                 await menu(reaction.emoji[0], msg)
                 await bmsg.delete()
 
 
-client.run('xxxxx')
+client.run('xxxxxxx')
