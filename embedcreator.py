0d3727d27a981c2686092c00ec4809abea4b47a0
LordDevpy <66754197+LordDevpy@users.noreply.github.com>
2020-08-25 09:26:17 -0300

    Update embedcreator.py

--- embedcreator.py
+++ embedcreator.py
@@ -13,7 +13,7 @@ async def embcreate(msg, message, error):
             desc2 = message.find(']', desc1)
             embed = discord.Embed(title=message[title1+1:title2],
                                   color=int(choice(['1752220', '3066993', '10181046', '2123412', '15158332', '15105570'])))
-            if str(message).count('[') == 2:
+            if str(message).count('[') >= 2:
                 await msg.channel.send(embed=discord.Embed(title=message[title1+1:title2],
                                       color=int(choice(['1752220', '3066993', '10181046', '2123412', '15158332', '15105570'])),
                                       description=message[desc1+1:desc2]))
